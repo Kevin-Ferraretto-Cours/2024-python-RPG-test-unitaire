6f147eff20ed4db183c81eb3b263945a128f988a
MrSuricate <kevindu032270@gmail.com>
2024-10-22 16:13:20 +0200

    add method attack

--- src/personage.py
+++ src/personage.py
@@ -1,3 +1,9 @@
 class Personage:
+    def __init__(self):
+        self.hp = 10
+
     def get_hp(self):
-        return 10
+        return self.hp
+
+    def attaquer(self, defenseur):
+        defenseur.hp = 9
--- test/test_rpg.py
+++ test/test_rpg.py
@@ -6,7 +6,13 @@ from src.personage import Personage
 class MyTestCase(unittest.TestCase):
     def test_10_hp_initiaux(self):
         personage = Personage()
-        self.assertEqual(10, personage.get_hp())  # add assertion here
+        self.assertEqual(10, personage.get_hp())
+
+    def test_attaquer_enleve_1_hp(self):
+        attaquant = Personage()
+        defenseur = Personage()
+        attaquant.attaquer(defenseur)
+        self.assertEqual(9, defenseur.get_hp())
 
 
 if __name__ == '__main__':
