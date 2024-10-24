04c2a601c6269e2b6617fa6295e7a8e1e5860c7c
Nezlik <killian.bousquet.leyendecker@gmail.com>
2024-10-24 10:21:10 +0200

    [RED] Tests

--- test/test_rpg.py
+++ test/test_rpg.py
@@ -37,5 +37,26 @@ class MyTest_Case(unittest.TestCase):
         attaquant.attaquer(defenseur) 
         self.assertEqual(0, defenseur.get_hp())  
 
+    def test_resurrection_personnage(self):
+        attaquant = Personnage()
+        defenseur = Personage()
+        for _ in range(10):
+            attaquant.attaquer(defenseur)
+        defenseur.resurrection()
+        self.assertEqual(1, defenseur.get_hp())
+        self.assertTrue(defenseur.est_vivant())
+
+    def test_resurrection_personnage_deja_vivant(self):
+        attaquant = Personage()
+        attaquant.resurrection()
+        self.assertEqual(10, attaquant.get_hp())
+
+    def test_resurrection_personnage_deja_vivant_a_9hp(self):
+        attaquant = Personage()
+        defenseur = Personage()
+        attaquant.attaquer(defenseur)
+        defenseur.resurrection()
+        self.assertEqual(9, defenseur.get_hp())
+        
 if __name__ == '__main__':
     unittest.main()
