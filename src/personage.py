2938dfa9e2ee7c3c3ff66cc94f682c319b68d506
tduki <95369488+tduki@users.noreply.github.com>
2024-10-24 09:11:26 +0200

    tests crée

--- src/personage.py
+++ src/personage.py
@@ -1,9 +1,17 @@
 class Personage:
     def __init__(self):
         self.hp = 10
+        self.est_mort = False
 
     def get_hp(self):
         return self.hp
 
     def attaquer(self, defenseur):
-        defenseur.hp -= 1
+        if not defenseur.est_mort:
+            defenseur.hp -= 1
+            if defenseur.hp <= 0:
+                defenseur.hp = 0  
+                defenseur.est_mort = True
+
+    def est_vivant(self):
+        return not self.est_mort
--- test/test_rpg.py
+++ test/test_rpg.py
@@ -2,8 +2,7 @@ import unittest
 
 from src.personage import Personage
 
-
-class MyTestCase(unittest.TestCase):
+class MyTest_Case(unittest.TestCase):
     def test_10_hp_initiaux(self):
         personage = Personage()
         self.assertEqual(10, personage.get_hp())
@@ -21,6 +20,22 @@ class MyTestCase(unittest.TestCase):
         attaquant.attaquer(defenseur)
         self.assertEqual(8, defenseur.get_hp())
 
+    def test_personnage_meurt_a_0_hp(self):
+        attaquant = Personage()
+        defenseur = Personage()
+        for _ in range(10):
+            attaquant.attaquer(defenseur)
+        self.assertEqual(0, defenseur.get_hp())
+        self.assertFalse(defenseur.est_vivant())
+
+    def test_personnage_ne_peut_plus_perdre_de_hp_apres_mort(self):
+        attaquant = Personage()
+        defenseur = Personage()
+        for _ in range(10):
+            attaquant.attaquer(defenseur)
+        self.assertEqual(0, defenseur.get_hp())
+        attaquant.attaquer(defenseur) 
+        self.assertEqual(0, defenseur.get_hp())  
 
 if __name__ == '__main__':
     unittest.main()
